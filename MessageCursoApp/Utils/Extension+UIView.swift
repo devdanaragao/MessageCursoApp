4f8cec29fc242cf0a98d2246965554685792eae1
Danilo Aragão <danaragaodev@gmail.com>
2024-01-16 15:33:37 +0000

    Added Extension + UIView.

--- MessageCursoApp/Utils/Extension+UIView.swift
+++ MessageCursoApp/Utils/Extension+UIView.swift
@@ -10,6 +10,10 @@ import UIKit
 
 extension UIView {
     func pin(to superView: UIView){
-        
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.topAnchor.constraint(equalTo: superView.topAnchor).isActive = true
+        self.leadingAnchor.constraint(equalTo: superView.leadingAnchor).isActive = true
+        self.trailingAnchor.constraint(equalTo: superView.trailingAnchor).isActive = true
+        self.bottomAnchor.constraint(equalTo: superView.bottomAnchor).isActive = true
     }
 }
